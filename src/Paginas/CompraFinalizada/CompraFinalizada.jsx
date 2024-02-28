767e219d8d1fc5d245a41f8429a1e23194a19265
bellinzona <mtbellinzona@gmail.com>
2024-02-28 12:55:54 -0300

    prueba ultima pantalla

--- src/Paginas/CompraFinalizada/CompraFinalizada.jsx
+++ src/Paginas/CompraFinalizada/CompraFinalizada.jsx
@@ -5,52 +5,9 @@ import { collection, addDoc } from 'firebase/firestore'
 import "./CompraFinalizada.css"
 
 export const CompraFinalizada = () => {
-  const [paymentInfo, setPaymentInfo] = useState(null);
+  const [paymentInfo, setPaymentInfo] = useState([]);
 
 
-  useEffect(() => {
-
-    fetch(`https://bs-i4ni.onrender.com/CompraFinalizada`)
-      .then(response => response.json())
-      .then(data => setPaymentInfo(data))
-      .catch(error => console.error('Error al obtener la información del pago:', error));
-
-
-
-  }, []);
-
-
-
-  useEffect(() => {
-    const guardarInformacion = async () => {
-      if (paymentInfo && paymentInfo.RequestBodyInfo) {
-        const clienteData = {
-          Nombre: paymentInfo.RequestBodyInfo.nombre,
-          Apellido: paymentInfo.RequestBodyInfo.apellido,
-          Producto: paymentInfo.RequestBodyInfo.description,
-          Local: paymentInfo.RequestBodyInfo.local,
-          Horario: paymentInfo.RequestBodyInfo.horario,
-          telefono: paymentInfo.RequestBodyInfo.telefono,
-          IdCompra: paymentInfo.QueryParams.payment_id
-        };
-  
-        try {
-          // Referencia a la colección en tu base de datos
-          const pagosCollection = collection(db, 'Pagos');
-  
-          // Agregar un nuevo documento con la información del pago
-          const nuevoPago = await addDoc(pagosCollection, clienteData);
-  
-          
-        } catch (error) {
-          console.error('Error al guardar la información del pago:', error);
-        }
-      }
-    };
-  
-    guardarInformacion();
-  }, [paymentInfo]);
-  
 
 
   return (
@@ -60,18 +17,7 @@ export const CompraFinalizada = () => {
 
       <h3 className='aaa'>Tu compra se realizo con exito</h3>
 
-      {paymentInfo && paymentInfo.RequestBodyInfo && (
-        <>
-          <p>Nombre: {paymentInfo?.RequestBodyInfo?.nombre ?? 'No disponible'}</p>
-          <p>Producto: {paymentInfo?.RequestBodyInfo?.description ?? 'No disponible'} ${paymentInfo?.RequestBodyInfo?.price ?? 'No disponible'}</p>
-          <p>Local: {paymentInfo?.RequestBodyInfo?.local ?? 'No disponible'}</p>
-          <p>{paymentInfo?.RequestBodyInfo?.horario ?? 'No disponible'}</p>
-          <p>Id de la compra: {paymentInfo?.QueryParams?.payment_id ?? 'No disponible'}</p>
-
-        </>
-      )}
-
-
+      
 
 
 
