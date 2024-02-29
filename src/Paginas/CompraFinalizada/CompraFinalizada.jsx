af81ded5eb2a604422df9e58c36c63998abeb481
bellinzona <mtbellinzona@gmail.com>
2024-02-29 15:58:44 -0300

    footer agregado

--- src/Paginas/CompraFinalizada/CompraFinalizada.jsx
+++ src/Paginas/CompraFinalizada/CompraFinalizada.jsx
@@ -5,9 +5,56 @@ import { collection, addDoc } from 'firebase/firestore'
 import "./CompraFinalizada.css"
 
 export const CompraFinalizada = () => {
-  const [paymentInfo, setPaymentInfo] = useState([]);
+  const [paymentInfo, setPaymentInfo] = useState(null);
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const collectionStatus = urlParams.get('collection_status');
+  const paymentId = urlParams.get('payment_id');
 
 
+  useEffect(() => {
+
+    fetch(`https://bs-i4ni.onrender.com/CompraFinalizada`)
+      .then(response => response.json())
+      .then(data => setPaymentInfo(data))
+      .catch(error => console.error('Error al obtener la información del pago:', error));
+
+
+
+  }, []);
+
+
+
+  useEffect(() => {
+    const guardarInformacion = async () => {
+      if (paymentInfo && paymentInfo.RequestBodyInfo) {
+        const clienteData = {
+          Nombre: paymentInfo.RequestBodyInfo.nombre,
+          Apellido: paymentInfo.RequestBodyInfo.apellido,
+          Producto: paymentInfo.RequestBodyInfo.description,
+          Local: paymentInfo.RequestBodyInfo.local,
+          Horario: paymentInfo.RequestBodyInfo.horario,
+          telefono: paymentInfo.RequestBodyInfo.telefono,
+          IdCompra: paymentInfo.QueryParams.payment_id
+        };
+  
+        try {
+          // Referencia a la colección en tu base de datos
+          const pagosCollection = collection(db, 'Pagos');
+  
+          // Agregar un nuevo documento con la información del pago
+          const nuevoPago = await addDoc(pagosCollection, clienteData);
+  
+          
+        } catch (error) {
+          console.error('Error al guardar la información del pago:', error);
+        }
+      }
+    };
+  
+    guardarInformacion();
+  }, [paymentInfo]);
+  
 
 
   return (
@@ -17,7 +64,18 @@ export const CompraFinalizada = () => {
 
       <h3 className='aaa'>Tu compra se realizo con exito</h3>
 
-      
+      {paymentInfo && paymentInfo.RequestBodyInfo && (
+        <>
+          <p>Nombre: {paymentInfo?.RequestBodyInfo?.nombre ?? 'No disponible'}</p>
+          <p>Producto: {paymentInfo?.RequestBodyInfo?.description ?? 'No disponible'} ${paymentInfo?.RequestBodyInfo?.price ?? 'No disponible'}</p>
+          <p>Local: {paymentInfo?.RequestBodyInfo?.local ?? 'No disponible'}</p>
+          <p>{paymentInfo?.RequestBodyInfo?.horario ?? 'No disponible'}</p>
+          <p>Id de la compra: {paymentInfo?.QueryParams?.payment_id ?? 'No disponible'}</p>
+
+        </>
+      )}
+
+
 
 
 
--- src/Paginas/Home.jsx
+++ src/Paginas/Home.jsx
@@ -13,6 +13,51 @@ export const Home = () => {
         <Carrousel></Carrousel>
 
         <Productos></Productos>
+
+
+        <div className='FooterMain' id='footer'>
+
+<div className="informaciones">
+    <h3>Contacto :</h3>
+
+    <p>Bubbamilanesas912@gmail.com</p>
+    <p>📱 1136181253</p>
+    
+
+
+</div>
+
+<div className="informaciones">
+    <h3>Locales :</h3>
+
+    <p>Curappaligue 47</p>
+    <p>Avenida Rivadavia 47</p>
+
+
+
+</div>
+
+<div className="informaciones">
+    <h3>Redes :</h3>
+
+    <p>@BubbaMilanesas</p>
+
+    
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+</div>
         
        
 
